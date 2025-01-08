7f042b32def13b8f7c3b469886deaaf2dd77efe2
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-08 14:15:05 -0300

    feat: create exceptions and function for raising exceptions

---
+++ utils.py
@@ -0,0 +1,17 @@
+from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
+from datetime import datetime
+
+
+def data_processing(team: dict):
+    current_date = datetime.now()
+    first_cup = datetime.strptime(team["first_cup"], "%Y-%m-%d")
+    possible_cups = round((int(first_cup.year) - int(current_date.year)) / 4, 0)
+
+    if team["titles"] < 0:
+        raise NegativeTitlesError("titles cannot be negative")
+
+    if int(first_cup.year) < 1930 or (int(first_cup.year) - 1930) % 4 != 0:
+        raise InvalidYearCupError("there was no world cup this year")
+
+    if team["titles"] > possible_cups:
+        raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
