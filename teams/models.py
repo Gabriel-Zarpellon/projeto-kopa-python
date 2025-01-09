30c1bf08eedcf0e48c758a731d7c726e36668453
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-09 13:01:41 -0300

    fix: add unique to fifa_code attribute

--- teams/models.py
+++ teams/models.py
@@ -5,7 +5,7 @@ class Team(models.Model):
     name = models.CharField(max_length=30)
     titles = models.IntegerField(blank=True, null=True, default=0)
     top_scorer = models.CharField(max_length=50)
-    fifa_code = models.CharField(max_length=3)
+    fifa_code = models.CharField(max_length=3, unique=True)
     first_cup = models.DateField(blank=True, null=True)
 
     def __repr__(self):
