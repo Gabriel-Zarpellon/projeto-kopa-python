34bdf972b1030159c6cb34cf246a34e726c1b63c
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-09 15:55:50 -0300

    feat: install and config DRF and create view for posting team and reading all teams

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, Request, status
+from .models import Team
+from django.forms.models import model_to_dict
+from utils import data_processing
+from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
 
-# Create your views here.
+
+class TeamView(APIView):
+    def post(self, request: Request) -> Response:
+        try:
+            data_processing(**request.data)
+        except NegativeTitlesError:
+            return Response(
+                {"error": "titles cannot be negative"}, status.HTTP_400_BAD_REQUEST
+            )
+        except InvalidYearCupError:
+            return Response(
+                {"error": "there was no world cup this year"},
+                status.HTTP_400_BAD_REQUEST,
+            )
+        except ImpossibleTitlesError:
+            return Response(
+                {"error": "impossible to have more titles than disputed cups"},
+                status.HTTP_400_BAD_REQUEST,
+            )
+
+        team = Team.objects.create(**request.data)
+
+        return Response(model_to_dict(team), status.HTTP_201_CREATED)
+
+    def get(self, request: Request) -> Response:
+        teams_dict = [model_to_dict(team) for team in Team.objects.all()]
+
+        return Response(teams_dict)
